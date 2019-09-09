e863c7783256a5bf92f63a327878e9fb6b14bb6a
Fredrik Vraalsen <fredrik@vraalsen.no>
2019-09-10 00:45:17 +0200

    Update exercise 6

--- 6_all_together/README.md
+++ 6_all_together/README.md
@@ -7,3 +7,6 @@ Exercise 6 - All Together Now
 - Start LocalStack, see top level [README](../README.md)
 
 ## Exercise
+
+Implement the temperature event pipeline as shown in
+![System diagram](system_diagram.png)
---
+++ 6_all_together/handler.py
@@ -0,0 +1,51 @@
+import boto3
+import json
+
+from boto3.dynamodb.conditions import Key
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url='http://localhost:4569')
+temperatures_table = dynamodb.Table('temperatures')
+
+
+def receive_event(event, context):
+    temperature = json.loads(event['body'])['temperature']
+
+    item = {
+        'location': 'Oslo',
+        'temperature': Decimal(temperature)
+    }
+
+    temperatures_table.put_item(Item=item)
+
+    return {
+        'statusCode': 200
+    }
+
+
+def run_pipeline(event, context):
+    pass
+
+
+def filter_temperature(event, context):
+    pass
+
+
+def enrich_location(event, context):
+    pass
+
+
+def write_temperature(event, context):
+    pass
+
+
+def get_temperatures(event, context):
+    items = temperatures_table.scan()['Items']
+    for item in items:
+        item['temperature'] = float(item['temperature'])
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(items),
+        'headers': {'Content-Type': 'application/json'}
+    }

818f947e1aedd7d48c435a798f02a6ef6f4386bf
Fredrik Vraalsen <fredrik@vraalsen.no>
2019-09-09 18:51:51 +0200

    Solutions for exercise 4

--- 4_event_processing/handler.py
+++ 4_event_processing/handler.py
@@ -15,9 +15,15 @@ def handle_sns(event, context):
     # See https://docs.aws.amazon.com/lambda/latest/dg/with-sns.html
 
     # TODO Implement event handler for SNS topic
+    for record in event["Records"]:
+        msg = record["Sns"]["Message"]
+        logger.info(f"Got SQS event: {msg}")
 
 def handle_kinesis(event, content):
     # See https://docs.aws.amazon.com/lambda/latest/dg/with-kinesis.html
 
     # TODO Implement event handler for Kinesis stream
     # Hint: Use base64.b64decode to decode event data
+    for record in event["Records"]:
+        msg = base64.b64decode(record["kinesis"]["data"]).decode("utf-8")
+        logger.info(f"Got Kinesis event: {msg}")
